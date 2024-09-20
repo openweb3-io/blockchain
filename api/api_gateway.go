3004c2513797ed457489cb5ec2fa2cb59f02a5f1
Pharaoh <pharaoh@mtsocialdao.com>
2024-09-21 02:20:43 +0800

    fix: update api gateway

--- api/api_gateway.go
+++ api/api_gateway.go
@@ -9,21 +9,21 @@ type ApiGateway interface {
 	GetApi(ctx context.Context, network string) (ApiClient, error)
 }
 
-type DefaultTransferGateway struct {
+type DefaultApiGateway struct {
 	apis map[string]ApiClient
 }
 
-func NewDefaultTransferGateway() *DefaultTransferGateway {
-	return &DefaultTransferGateway{
+func NewDefaultApiGateway() *DefaultApiGateway {
+	return &DefaultApiGateway{
 		apis: make(map[string]ApiClient),
 	}
 }
 
-func (g *DefaultTransferGateway) Register(ctx context.Context, network string, api ApiClient) {
+func (g *DefaultApiGateway) Register(ctx context.Context, network string, api ApiClient) {
 	g.apis[network] = api
 }
 
-func (g *DefaultTransferGateway) GetApi(ctx context.Context, network string) (ApiClient, error) {
+func (g *DefaultApiGateway) GetApi(ctx context.Context, network string) (ApiClient, error) {
 	api, ok := g.apis[network]
 	if !ok {
 		return nil, fmt.Errorf("api not found for network: %s", network)
